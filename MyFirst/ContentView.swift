0e0159a2e8cb18a88aa671222021aa920659aa76
suzuim <p19028is@gm.tsuda.ac.jp>
2021-12-27 18:59:30 +0900

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by 今井涼葉 on 2021/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var outputText = "Hello, World!"
+    
+    var body: some View {
+        
+        VStack{
+            Text(outputText)
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                
+                outputText = "Hi, Swift!"
+                
+            }) {
+                Text("切り替えボタン")
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(Color.blue)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by 今井涼葉 on 2021/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
